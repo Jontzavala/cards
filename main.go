27ef527d3aaa1ca5b513a3db3a6b62658049d4bd
Jonathan Zavala <jontzavala@gmail.com>
2023-07-25 16:23:29 -0500

    Multiple Return Values

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 
 
 func main() {
-	cards := deck{newCard(), newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
+	
+
 }
 
-func newCard() string {
-	return "Five of Diamonds"
-}
+
